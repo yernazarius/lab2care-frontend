2f86d2ad4e7116b111fa943ca835c11121f8d86f
yernazarius <ernzaryakupov04@gmail.com>
2023-11-06 22:32:57 +0600

    first commit

---
+++ src/routes/cart/+page.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+</script>
+
+<body>
+  <div class="h-screen bg-gray-100 pt-20">
+    <h1 class="mb-10 text-center text-2xl font-bold">Cart Items</h1>
+    <div
+      class="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0"
+    >
+      <div class="rounded-lg md:w-2/3">
+        <!-- {#if cartItems.length === 0}
+          <p>Your cart is empty.</p>
+        {:else}
+          {#each cartItems as item} -->
+        <!-- sssstart -->
+        <div
+          class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start"
+        >
+          <img src="" alt="" class="w-full rounded-lg sm:w-40" />
+          <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+            <div class="mt-5 sm:mt-0">
+              <h2 class="text-lg font-bold text-gray-900">
+                <!-- {item.productTitle} --> Title
+              </h2>
+              <p class="mt-1 text-xs text-gray-700">
+                <!-- {item.productDescription}  --> description
+              </p>
+            </div>
+            <div
+              class="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6"
+            >
+              <div class="flex items-center border-gray-100">
+                <p class="">
+                  Quantity: <b>
+                    <!-- {item.quantity} -->
+                    quantity
+                  </b>
+                </p>
+              </div>
+              <div class="flex items-center space-x-4">
+                <p class="text-sm">
+                  $
+                  <!-- {item.productPrice} --> price
+                </p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- {/each} -->
+        <!-- {/if} -->
+        <!-- end -->
+      </div>
+      <!-- Sub total -->
+      <div
+        class="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3"
+      >
+        <!-- {#if cartItems.length === 0}
+          <p>Your cart is empty.</p>
+        {:else} -->
+        <div class="mb-2 flex justify-between">
+          <p class="text-gray-700">Subtotal</p>
+          <p class="text-gray-700">$ some $$$</p>
+        </div>
+        <div class="flex justify-between">
+          <p class="text-gray-700">Shipping</p>
+          <p class="text-gray-700">$4.99</p>
+        </div>
+        <hr class="my-4" />
+        <div class="flex justify-between">
+          <p class="text-lg font-bold">Total</p>
+          <div class="">
+            <p class="mb-1 text-lg font-bold">$</p>
+            <p class="text-sm text-gray-700">including VAT</p>
+          </div>
+        </div>
+        <a
+          href="/cart/order"
+          type="button"
+          class="text-center mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600"
+          >Check out</a
+        >
+        <!-- {/if} -->
+      </div>
+    </div>
+  </div>
+</body>
---
+++ src/routes/categories/+page.svelte
@@ -0,0 +1,75 @@
+<script>
+  import ProductCard from "$lib/components/ProductCard.svelte";
+  /** @type {import('./$types').PageData} */
+  export let data;
+  let wow = data.props;
+  let categoty_data = data.props.categoty_res;
+  console.log(categoty_data);
+</script>
+
+<section class="bg-white">
+  <div class="container px-6 py-8 mx-auto">
+    <div class="lg:flex lg:-mx-2">
+      <div class="space-y-3 lg:w-1/5 lg:px-2 lg:space-y-4">
+        {#each categoty_data as item}
+          <a
+            href="/"
+            class="block font-semibold text-gray-700 capitalize hover:underline"
+          >
+            <span class="ml-2">
+              {item.name}
+            </span>
+            <div class="flex">
+              <svg
+                fill="none"
+                height="26"
+                viewBox="0 0 24 24"
+                width="26"
+                xmlns="http://www.w3.org/2000/svg"
+                ><path
+                  d="m15 20c-.3 0-.5-.1-.7-.3-.4-.4-.4-1 0-1.4l2.3-2.3h-8.6c-1.3 0-2.6-.5-3.5-1.5-1-.9-1.5-2.2-1.5-3.5s.5-2.6 1.5-3.5c.9-1 2.2-1.5 3.5-1.5h1c.6 0 1 .4 1 1s-.4 1-1 1h-1c-.8 0-1.6.3-2.1.9-.6.5-.9 1.3-.9 2.1s.3 1.6.9 2.1c.5.6 1.3.9 2.1.9h8.6l-2.3-2.3c-.4-.4-.4-1 0-1.4s1-.4 1.4 0l4 4c.1.1.2.2.2.3s.1.2.1.4c0 .1 0 .3-.1.4 0 .1-.1.2-.2.3l-4 4c-.2.2-.4.3-.7.3z"
+                  fill="rgb(0,0,0)"
+                /></svg
+              >
+              <p class="mb-2 mt-1 text-sm font-medium text-gray-500">
+                {item.description}
+              </p>
+            </div>
+          </a>
+        {/each}
+      </div>
+
+      <div class="mt-6 lg:mt-0 lg:px-2 lg:w-4/5">
+        <div
+          class="flex items-center justify-between text-sm tracking-widest uppercase"
+        >
+          <p class="text-gray-500">Items</p>
+          <div class="flex items-center">
+            <p class="text-gray-500">Sort</p>
+            <select
+              class="font-medium text-gray-700 bg-transparent focus:outline-none"
+            >
+              <option value="#">Recommended</option>
+              <option value="#">Size</option>
+              <option value="#">Price</option>
+            </select>
+          </div>
+        </div>
+
+        <div
+          class="grid grid-flow-row gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+        >
+          {#each wow.data as item}
+            <ProductCard
+              productImage={item.image}
+              productPrice={item.price}
+              productTitle={item.name}
+              productDescription={item.description}
+              productId={item.id}
+            />
+          {/each}
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
